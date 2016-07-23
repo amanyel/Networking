8bd665ddf9e32f05f3c338fc6a71ab1bf9037ba6
Amany EL Romeh <amanyel@users.noreply.github.com>
2016-07-23 22:53:22 +0300

    All IP Address

---
+++ MyAllIpAddresses.java
@@ -0,0 +1,23 @@
+
+package networking;
+/**
+ *
+ * @author Amany
+ */
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MyAllIpAddresses {
+
+    public static void main(String a[]){
+
+        try {
+            InetAddress[] myHost = InetAddress.getAllByName("www.google.com");
+            for(InetAddress host:myHost){
+                System.out.println(host.getHostAddress());
+            }
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
