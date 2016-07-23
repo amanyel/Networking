dfd1f861ac5614e44f1823a834f81abb9f0739fa
Amany EL Romeh <amanyel@users.noreply.github.com>
2016-07-23 22:53:52 +0300

    IP Address

---
+++ MyIpAddress.java
@@ -0,0 +1,20 @@
+package networking;
+/**
+ *
+ * @author Amany
+ */
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MyIpAddress {
+
+    public static void main(String a[]){
+
+        try {
+            InetAddress ipAddr = InetAddress.getLocalHost();
+            System.out.println(ipAddr.getHostAddress());
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
