0bc0dca17c4a8d99604fa5fbe56a5a5c3f94ea5f
Amany EL Romeh <amanyel@users.noreply.github.com>
2016-07-30 22:59:26 +0300

    Simple Url

--- SimpleUrl.java
+++ SimpleUrl.java
@@ -1,20 +1,17 @@
-
-package networking;
-/**
- *
- * @author Amany
- */
 import java.net.MalformedURLException;
-import java.net.URL;
-
-public class SimpleUrl {
-    public static void main(String a[]){
-        try {
-            String myUrl = "http://www.goggle.com";
-            URL url = new URL(myUrl);
-            System.out.println(url.toString());
-        } catch (MalformedURLException ex) {
-            ex.printStackTrace();
-        }
-    }
-}
+ import java.net.URL;
+ 
+  public class SimpleUrl 
+  { 
+  public static void main(String a[]){
+  
+   try { 
+   String myUrl = "http://www.google.com";
+    URL url = new URL(myUrl); 
+    System.out.println(url.toString());
+    
+     } catch (MalformedURLException ex) { 
+     ex.printStackTrace(); 
+     }
+      }
+       }
